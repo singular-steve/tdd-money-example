44519fffc6ae16ac6942aa48156c859c813e62fb
singular-steve <singular1.steve@gmail.com>
2020-01-13 18:16:36 +0900

    add Bank, Expression and Sum class for Money adding

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-public class Money {
+public class Money implements Expression {
   protected int amount;
   protected String currency;
 
@@ -32,5 +32,13 @@ public class Money {
 
   public String toString() {
     return amount + " " + currency;
+  }
+
+  public Expression plus(Money addend) {
+    return new Sum(this, addend);
+  }
+
+  public Money reduce(String to) {
+    return this;
   }
 }

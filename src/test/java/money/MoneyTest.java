f80ce20f63b5ce67cb96b2e0208bd0228e496538
singular-steve <singular1.steve@gmail.com>
2020-01-14 11:21:18 +0900

    add rate function

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -1,5 +1,6 @@
 package money;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static junit.framework.TestCase.*;
@@ -63,5 +64,24 @@ public class MoneyTest {
     Bank bank = new Bank();
     Money result = bank.reduce(Money.dollar(1), "USD");
     assertEquals(Money.dollar(1), result);
+  }
+
+  @Test
+  public void testReduceMoneyDifferentCurrency() {
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Money result = bank.reduce(Money.franc(2), "USD");
+    assertEquals(Money.dollar(1), result);
+  }
+
+  @Test
+  @Ignore
+  public void testArrayEquals() {
+    assertEquals(new Object[] {"abc"}, new Object[] {"abc"});
+  }
+
+  @Test
+  public void testIdentityRate() {
+    assertEquals(1, new Bank().rate("USD", "USD"));
   }
 }
